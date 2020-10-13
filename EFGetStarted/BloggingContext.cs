c80e8ae1cec62d7dfad809aec8fa0ec611fa3a04
roamschemer <roamschemer@gmail.com>
2020-10-13 10:24:04 +0900

    プロジェクト ファイルを追加します。

---
+++ EFGetStarted/BloggingContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace EFGetStarted
+{
+    public class BloggingContext : DbContext
+    {
+        public static readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter((category, level) =>
+                    category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information)
+                .AddConsole();
+        });
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => 
+            options.UseLoggerFactory(loggerFactory)
+                   .EnableSensitiveDataLogging()
+                   .UseSqlite("Data Source=blogging.db");
+    }
+}
